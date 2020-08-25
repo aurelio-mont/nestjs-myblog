ce5de1c9a4108fe59804d79daf5a424b4c96431d
aurelio-mont <aurelio.mont25@gmail.com>
2020-08-25 14:14:39 -0500

    Terminando User

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,56 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto, EditUserDto } from './dtos';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(
+        private readonly userService: UserService
+    ) {}
+
+    @Get()
+    async getMany() {
+        const data = await this.userService.getMany();
+        return { data };
+    }
+    
+    @Get(':id')
+    async getOne(
+        @Param() id: number
+    ) {
+        const data = await this.userService.getOne(id);
+        return { data };
+    }
+
+    @Post()
+    async createOne(
+        @Body() dto: CreateUserDto
+    ) {
+        const data = await this.userService.createOne(dto);
+        return { 
+            message: 'Usuario Creado',    
+            data
+        };
+    }
+    
+    @Put(':id')
+    async editOne(
+        @Param() id: number,
+        @Body() dto: EditUserDto
+    ) {
+      const data = await this.userService.editOne(id, dto);
+      return { 
+        message: 'Usuario Modificado',    
+        data
+    };  
+    }
+
+    @Delete(':id')
+    async deleteOne(
+        @Param() id: number
+    ) {
+        const data = await this.userService.deleteOne(id);
+        return { message: "Usuario eliminado", data }
+    }
+
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, BadRequestException } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from './entities';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateUserDto, EditUserDto } from './dtos';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+
+    constructor(
+        @InjectRepository(User)
+        private readonly userRepository: Repository<User>
+    ) {}
+
+    async getMany() {
+        return await this.userRepository.find();
+    }
+
+    async getOne(id: number) {
+        const user = await this.userRepository.findOne(id);
+        if (!user) {
+            throw new NotFoundException('El Usuario no existe');
+        }
+
+        return user;
+    }
+
+    async createOne(dto: CreateUserDto) {
+        const userExist = await this.userRepository.findOne({ email: dto.email });
+        if (userExist) {
+            throw new BadRequestException('El Usuario ya esta registrado');
+        }
+        const newUser = this.userRepository.create(dto);
+        const user =  await this.userRepository.save(newUser);
+
+        delete user.password;
+        return user;
+    }
+
+    async editOne(id: number, dto: EditUserDto) {
+        const user = await this.getOne(id);
+        const editUser = Object.assign(user, dto);
+        return await this.userRepository.save(editUser);
+    }
+
+    async deleteOne(id: number) {
+        const user = await this.getOne(id);
+        return await this.userRepository.remove(user);
+    }
+}
